eccb7f173b26f95f68bd5bff7e6608d22245f4f7
Oren <holtzmanoren99@gmail.com>
2024-08-30 11:31:32 +0300

    more efficient quotes

---
+++ src/components/QuotesData.js
@@ -0,0 +1,11 @@
+// quotesData.js
+const quotes = [
+    { id: 1, text: "The only limit to our realization of tomorrow is our doubts of today.", author: "Franklin D. Roosevelt" },
+    { id: 2, text: "The purpose of our lives is to be happy.", author: "Dalai Lama" },
+    { id: 3, text: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
+    { id: 4, text: "Get busy living or get busy dying.", author: "Stephen King" },
+    { id: 5, text: "You have within you right now, everything you need to deal with whatever the world can throw at you.", author: "Brian Tracy" }
+  ];
+  
+  export default quotes;
+  
